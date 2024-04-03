98560b70a1074e6d97774004ccaad85b510537cd
Ashish Mathew Abraham Kuttenkeril <ashishmathew.77@gmail.com>
2024-04-02 21:27:18 -0400

    first commit

---
+++ Hello/hello.py
@@ -0,0 +1,30 @@
+print("Hello", end=",") # This is my first piece of code
+print("World")
+
+"""This is 
+a multiline
+comment
+"""
+##variables
+ # Numbers - whole numbers, floats, complex numbers
+
+
+num  = 100
+print(num)
+
+print(1.234567789)
+
+print(complex(10,20))
+
+
+## booleans
+print(True)
+
+##Strings
+print("Ashish")
+print('Mathew')
+print('''Abrhaham
+Kuttenkeril
+      ''') ##multinline
+name = "Ashish"
+print(len(name))
